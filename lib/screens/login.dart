8fed4548542a6a43cba2187b5f5b25c11df86148
Apil <ac.apil00chand@gmail.com>
2023-05-18 19:45:48 +0545

    login

--- lib/screens/login.dart
+++ lib/screens/login.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:pathsala/screens/Admin/Dashboard.dart';
 import 'package:pathsala/screens/Parent/Pdashboard.dart';
@@ -16,11 +15,7 @@ class LoginPage extends StatefulWidget {
 class _LoginPageState extends State<LoginPage> {
   final TextEditingController _usernameController = TextEditingController();
   final TextEditingController _passwordController = TextEditingController();
-  final FirebaseAuth _auth = FirebaseAuth.instance;
   final FirebaseFirestore _firestore = FirebaseFirestore.instance;
-
-  final List<String> _roles = ['student', 'parent', 'teacher', 'admin'];
-  String _selectedRole = 'student';
 
   @override
   Widget build(BuildContext context) {
@@ -28,107 +23,105 @@ class _LoginPageState extends State<LoginPage> {
       appBar: AppBar(
         title: const Text('Login'),
       ),
-      body: SingleChildScrollView(child:
-      Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            DropdownButtonFormField<String>(
-              value: _selectedRole,
-              decoration: const InputDecoration(
-                labelText: 'Role',
-                border: OutlineInputBorder(),
-              ),
-              items: _roles.map((role) {
-                return DropdownMenuItem<String>(
-                  value: role,
-                  child: Text(role),
-                );
-              }).toList(),
-              onChanged: (value) {
-                setState(() {
-                  _selectedRole = value!;
-                });
-              },
-            ),
-            const SizedBox(height: 16.0),
-            TextField(
-              controller: _usernameController,
-              decoration: const InputDecoration(
-                labelText: 'Username',
-                border: OutlineInputBorder(),
-              ),
-            ),
-            const SizedBox(height: 16.0),
-            TextField(
-              controller: _passwordController,
-              decoration: const InputDecoration(
-                labelText: 'Password',
-                border: OutlineInputBorder(),
-              ),
-              obscureText: true,
-            ),
-            const SizedBox(height: 16.0),
-            ElevatedButton(
-              onPressed: () {
-                _login();
-              },
-              child: const Text('Login'),
-            ),
-          ],
+      body: Container(
+        color: Colors.blueGrey, // Set the background color
+        child: Center(
+          child: LayoutBuilder(
+            builder: (BuildContext context, BoxConstraints constraints) {
+              final double containerWidth = constraints.maxWidth > 1007
+                  ? 500.0
+                  : constraints.maxWidth * 0.8;
+
+              return Container(
+                width: containerWidth,
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(10.0),
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.black.withOpacity(0.2),
+                      blurRadius: 5.0,
+                      offset: const Offset(0, 3),
+                    ),
+                  ],
+                ),
+                child: Padding(
+                  padding: const EdgeInsets.all(16.0),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      const SizedBox(height: 16.0),
+                      TextField(
+                        controller: _usernameController,
+                        decoration: const InputDecoration(
+                          labelText: 'Username',
+                          border: OutlineInputBorder(),
+                        ),
+                      ),
+                      const SizedBox(height: 16.0),
+                      TextField(
+                        controller: _passwordController,
+                        decoration: const InputDecoration(
+                          labelText: 'Password',
+                          border: OutlineInputBorder(),
+                        ),
+                        obscureText: true,
+                      ),
+                      const SizedBox(height: 16.0),
+                      ElevatedButton(
+                        onPressed: () {
+                          _login();
+                        },
+                        child: const Text('Login'),
+                      ),
+                    ],
+                  ),
+                ),
+              );
+            },
+          ),
         ),
       ),
-    ));
+    );
   }
 
-  void _login() async {
-    final String enteredusername = _usernameController.text.trim();
-    final String enteredpassword = _passwordController.text.trim();
+ 
 
-    UserCredential userCredential;
-    DocumentSnapshot userSnapshot;
+  void _login() async {
+    final String enteredUsername = _usernameController.text.trim();
+    final String enteredPassword = _passwordController.text.trim();
 
     try {
-      userCredential = await _auth.signInWithEmailAndPassword(
-        email: enteredusername,
-        password: enteredpassword,
-      );
+      final QuerySnapshot snapshot = await _firestore
+          .collection('User')
+          .where('username', isEqualTo: enteredUsername)
+          .where('password', isEqualTo: enteredPassword)
+          .limit(1)
+          .get();
 
-      final User? user = userCredential.user;
-      if (user != null) {
-        final String userId = user.uid;
-        final DocumentReference userRef = _firestore.collection('User').doc(userId);
-        userSnapshot = await userRef.get();
+      if (snapshot.docs.isNotEmpty) {
+        final DocumentSnapshot userSnapshot = snapshot.docs.first;
+        final String role = userSnapshot['role'];
+        final String userId = userSnapshot.id;
 
-        if (userSnapshot.exists) {
-          final String role = userSnapshot['role'];
-          final String username = userSnapshot['username'];
-          final String password = userSnapshot['password'];
-
-          if (username == enteredusername && password == enteredpassword) {
-            switch (role) {
-              case 'student':
-                _navigateToStudentDashboard(userId);
-                break;
-              case 'parent':
-                _navigateToParentDashboard();
-                break;
-              case 'teacher':
-                _navigateToTeacherDashboard(userId);
-                break;
-              case 'admin':
-                _navigateToAdminDashboard();
-                break;
-              default:
-                _showAlertDialog('Invalid role');
-            }
-          } else {
-            _showAlertDialog('Invalid username or password');
-          }
-        } else {
-          _showAlertDialog('User not found');
+        switch (role) {
+          case 'student':
+            _navigateToStudentDashboard(userId);
+            break;
+          case 'parent':
+            _navigateToParentDashboard(userId);
+            break;
+          case 'teacher':
+            _navigateToTeacherDashboard(userId);
+            break;
+          case 'admin':
+            _navigateToAdminDashboard(userId);
+            break;
+          default:
+            _showAlertDialog('Invalid role');
         }
+      } else {
+        _showAlertDialog('Invalid username or password');
       }
     } catch (e) {
       _showAlertDialog(e.toString());
@@ -139,14 +132,12 @@ class _LoginPageState extends State<LoginPage> {
     Navigator.pushReplacement(
       context,
       MaterialPageRoute(
-        builder: (context) => StudentDashboard(
-           studentId: userId,
-        ),
+        builder: (context) => StudentDashboard(studentId: userId),
       ),
     );
   }
 
-  void _navigateToParentDashboard() {
+  void _navigateToParentDashboard(String userId) {
     Navigator.pushReplacement(
       context,
       MaterialPageRoute(
@@ -159,14 +150,12 @@ class _LoginPageState extends State<LoginPage> {
     Navigator.pushReplacement(
       context,
       MaterialPageRoute(
-        builder: (context) => TeacherDashboardScreen(
-          teacherId: userId,
-        ),
+        builder: (context) => TeacherDashboardScreen(teacherId: userId),
       ),
     );
   }
 
-  void _navigateToAdminDashboard() {
+  void _navigateToAdminDashboard(String userId) {
     Navigator.pushReplacement(
       context,
       MaterialPageRoute(
